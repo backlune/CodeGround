75443fad192043583817a1120ea368f87da3906b
Emil Backlund <emil.backl@gmail.com>
2017-10-09 22:13:40 +0200

    Added a coding dojo project and started a bowling game kata

---
+++ GodeGround/CodeGround.CodingDojo/BowlingGameKata/BowlingGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeGround.CodingDojo.BowlingGameKata
+{
+    internal class BowlingGame
+    {
+        private Player _player;
+
+        public BowlingGame()
+        {
+            _player = new Player();
+            Result.Scores.Add(_player, new List<int>());
+        }
+
+        public BowlingResult Result { get; internal set; } = new BowlingResult();
+
+        internal void Try(int result)
+        {
+            Result.Scores[_player].Add(result);
+        }
+    }
+
+    internal class BowlingResult
+    {
+        private readonly Dictionary<Player, List<int>> scores = new Dictionary<Player, List<int>>();
+
+        public Dictionary<Player, List<int>> Scores { get => scores; }
+        public bool GameComplete => Scores.All(x => x.Value.Count() == 20);
+
+        internal int GetCurrentScore()
+        {
+            return scores.First().Value.Sum();
+        }
+    }
+}
---
+++ GodeGround/CodeGround.CodingDojo/BowlingGameKata/BowlingGameTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeGround.CodingDojo.BowlingGameKata
+{
+    
+    public class BowlingGameTests
+    {
+
+        [Fact]
+        public void ALineIsFinishedAfterTenFrames()
+        {
+            var sut = new BowlingGame();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sut.Try(1);
+                sut.Try(1);
+            }
+
+            var result = sut.Result;
+            Assert.True(result.GameComplete);
+        }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(4, 3, 7)]
+        [InlineData(8, 1, 9)]
+        public void WhenAllAreNotKnockedDownInAFrameTotalScoreIsTotalNumberOfPins(int tryOnePins, int tryTwoPins, int expectedResult)
+        {
+            var sut = new BowlingGame();
+
+            sut.Try(tryOnePins);
+            sut.Try(tryTwoPins);
+
+            Assert.Equal(expectedResult, sut.Result.GetCurrentScore());
+        }
+    }
+}
