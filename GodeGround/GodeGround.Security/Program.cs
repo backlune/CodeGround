083ae7f1b52d42ab2a9e39306196d89aa96e15d6
Emil Backlund <emil.b@bokio.se>
2020-12-08 11:35:08 +0100

    Trying pdf document scripting

--- GodeGround/GodeGround.Security/Program.cs
+++ GodeGround/GodeGround.Security/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -7,11 +8,23 @@ using System.Threading.Tasks;
 
 namespace GodeGround.Security
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         Console.ReadLine();
-      }
-   }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "C:\\Users\\emilb\\Downloads\\Faktura 33 Emil Accrual AB.pdf";
+            var unsecure_fileName = "C:\\Users\\emilb\\Downloads\\unsecure_Faktura 33 Emil Accrual AB.pdf";
+
+            using (var fs = File.Open(fileName, FileMode.Open))
+            using (var output = File.Open(unsecure_fileName, FileMode.OpenOrCreate))
+            using (var ms = PdfDocumentScripting.AddAutoPrint(fs))
+            {
+                ms.Position = 0;
+                ms.CopyTo(output);
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
 }
